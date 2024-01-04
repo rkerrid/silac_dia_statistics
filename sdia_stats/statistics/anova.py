9448b64a77900788466e04c6a3ee134efcc5e159
rkerrid <robbiejamesk@protonmail.com>
2024-01-04 22:12:39 +0100

    fixed anova directory handling

--- sdia_stats/statistics/anova.py
+++ sdia_stats/statistics/anova.py
@@ -10,18 +10,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sdia_stats.utils.manage_directories import create_directory
 
-# path = "G:/My Drive/Data/data/spikein data/"
-# meta = f"{path}meta.xlsx"
-# group = 'Treatment'
 
-def preform_anova(path, group, subset_cols):
+def preform_anova(path, group, subset_cols, title):
     meta = f'{path}meta.csv'
     meta_file = pd.read_csv(meta, sep=',')
     intensity_cols = subset_cols[1:]
-    # loader_total = alphastats.GenericLoader(f"{path}total_imputed.csv", 
-    #                                   intensity_column = '[Run]',
-    #                                    index_column="Protein.Group")
-
+    
     loader_light = alphastats.GenericLoader(f"{path}light.csv", 
                                       intensity_column = intensity_cols,
                                        index_column="Protein.Group")
@@ -29,12 +23,6 @@ def preform_anova(path, group, subset_cols):
     loader_nsp = alphastats.GenericLoader(f"{path}nsp.csv", 
                                       intensity_column = intensity_cols,
                                        index_column="Protein.Group")
-
-
-    # df_total = alphastats.DataSet(
-    #     loader = loader_total,
-    #     metadata_path = meta,
-    #     sample_column = 'Sample')
 
     df_light = alphastats.DataSet(
         loader = loader_light,
@@ -48,19 +36,10 @@ def preform_anova(path, group, subset_cols):
 
     # preprocess data, log2 transform
     df_nsp.preprocess(subset=True)
-    # df_total.preprocess(subset=True)
     df_light.preprocess(subset=True)
 
-    # total_anova = df_total.anova(column = group)
-
     nsp_anova = df_nsp.anova(column = group)
-
     light_anova  = df_light.anova(column = group)
 
-    # total_anova.to_csv(f'{path}total_anova.csv',sep=',')
-    
-    create_directory(f'{path}', 'anova_results')
-    
-    light_anova.to_csv(f'{path}/anova_results/light_anova.csv',sep=',')
-    nsp_anova.to_csv(f'{path}/anova_results/nsp_anova.csv',sep=',')
-# preform_anova(path, meta, group)
+    light_anova.to_csv(f'{path}light_anova.csv',sep=',')
+    nsp_anova.to_csv(f'{path}/nsp_anova.csv',sep=',')
--- sdia_stats/visualization/clustermap_anova.py
+++ sdia_stats/visualization/clustermap_anova.py
@@ -12,20 +12,13 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from icecream import ic
 
-def anova_clustermap(df, path, samples, title):
-    ic(df)
-    filtered = df[df['ANOVA_pvalue']<0.01]
-    ic(filtered)
+def anova_clustermap(df, path, samples, title, pval=0.01):
+    filtered = df[df['ANOVA_pvalue']<pval]
     cols = ['Protein.Group']+samples
-    ic(cols)
     filtered = filtered[cols]
     
-    ic(filtered)
     filtered.iloc[:,1:] = filtered.iloc[:,1:].apply(zscore, axis=1)
-    
     filtered = filtered.set_index('Protein.Group')
-    ic(filtered)
-    
     
     # Define the figure size, width and especially height to accommodate more y-axis labels
     height = len(filtered.index) * 0.3  # Adjust 0.3 to a smaller number if labels overlap, or larger for more space
@@ -33,29 +26,25 @@ def anova_clustermap(df, path, samples, title):
     
     g = sns.clustermap(filtered, figsize=(width, height), method='average', metric='euclidean',  cmap='viridis')
     
-    
-    # sns.heatmap(total_filtered.iloc[:400,:], annot=False, cmap='viridis', linewidths=.5, vmin=-1,vmax=1)
-    
     plt.setp(g.ax_heatmap.yaxis.get_majorticklabels(), rotation=-45, fontsize=6)
-    g.savefig(f'{path}cluster_{title}.pdf')
+    g.savefig(f'{path}cluster_{title}_{pval}.pdf')
     plt.show()
    
 def merge_data(df, anova):
     merged_df = pd.merge(df, anova, on='Protein.Group')
-    # merged_df = merged_df.iloc[:,1:]
-    ic(merged_df)
     return merged_df
 
-def plot_clustermap(path, samples):
+def plot_clustermap(path):
+    print(path)
     light_df = pd.read_csv(f'{path}light.csv',sep=',', index_col=False)
     nsp_df = pd.read_csv(f'{path}nsp.csv',sep=',', index_col=False)
     nsp_anova = pd.read_csv(f"{path}nsp_anova.csv", sep=',', index_col=False)
     light_anova = pd.read_csv(f"{path}light_anova.csv", sep=',', index_col=False)
     
-    
     nsp_merged = merge_data(nsp_df, nsp_anova)
     light_merged = merge_data(light_df, light_anova)
     
+    samples = light_df.columns.values.tolist()[1:]
     anova_clustermap(nsp_merged, path, samples, "nsp")
     anova_clustermap(light_merged, path, samples, "light")
 
@@ -65,8 +54,8 @@ if __name__ =='__main__':
     path = 'G:/My Drive/Data/data/eif4g optimization/imputed/normalized/for_anova/'
     path = 'G:/My Drive/Data/data/example poc/H/imputed/normalized/anova_results/'
     
-    samples = ['control_I', 'control_II', 'control_III', 'DFO_I','DFO_II','DFO_III']
+    # samples = ['control_I', 'control_II', 'control_III', 'DFO_I','DFO_II','DFO_III']
     # samples = ['08a_1','08a_2','08a_3','18a_1','18a_2','18a_3','28a_1','28a_2','28a_3']
     
-    plot_clustermap(path, samples)
+    plot_clustermap(path)
 
